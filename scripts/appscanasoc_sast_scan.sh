5659d947856b2f21551416aa696128b2c40979d5
Luke Santoro <lukes@hcl.com>
2026-04-23 15:40:56 -0400

    fixed auth issue in sast script

--- scripts/appscanasoc_sast_scan.sh
+++ scripts/appscanasoc_sast_scan.sh
@@ -29,8 +29,8 @@ else
 fi
 
 # Authenticate in ASOC
-asocToken=$(curl -k -s -X POST --header 'Content-Type:application/json' --header 'Accept:application/json' -d '{"KeyId":"'"$asocApiKeyId"'","KeySecret":"'"$asocApiKeySecret"'"}' "https://$serviceUrl/api/v4/Account/ApiKeyLogin" | grep -oP '(?<="Token":\ ")[^"]*')
-if [ -z "$asocToken" ]; then
+appscanToken=$(curl -k -s -X POST --header 'Content-Type:application/json' --header 'Accept:application/json' -d '{"KeyId":"'"$appscanApiKeyId"'","KeySecret":"'"$appscanApiKeySecret"'"}' "https://$serviceUrl/api/v4/Account/ApiKeyLogin" | grep -oP '(?<="Token":\ ")[^"]*')
+if [ -z "$appscanToken" ]; then
 	echo "The token variable is empty. Check the authentication process.";
     exit 1
 fi
@@ -38,7 +38,7 @@ fi
 irxFile=$(ls -t *.irx | head -n1)
 # Upload IRX file
 if [ -f "$irxFile" ]; then    
-    irxFileId=$(curl -k -s -X 'POST' "https://$serviceUrl/api/v4/FileUpload" -H 'accept:application/json' -H "Authorization:Bearer $asocToken" -H 'Content-Type:multipart/form-data' -F "uploadedFile=@$irxFile" | grep -oP '(?<="FileId":\ ")[^"]*');
+    irxFileId=$(curl -k -s -X 'POST' "https://$serviceUrl/api/v4/FileUpload" -H 'accept:application/json' -H "Authorization:Bearer $appscanToken" -H 'Content-Type:multipart/form-data' -F "uploadedFile=@$irxFile" | grep -oP '(?<="FileId":\ ")[^"]*');
     echo "$irxFile exist. It will be uploaded to ASoC. IRX file id is $irxFileId.";
 else
     echo "IRX file not identified.";
@@ -46,17 +46,17 @@ else
 fi
 
 # Scan start
-scanId=$(curl -s -k -X 'POST' "https://$serviceUrl/api/v4/Scans/Sast" -H 'accept:application/json' -H "Authorization:Bearer $asocToken" -H 'Content-Type:application/json' -d '{"AppId":"'"$appId"'","ApplicationFileId":"'"$irxFileId"'","ClientType":"user-site","EnableMailNotification":false,"Execute":true,"Locale":"en","Personal":false,"ScanName":"'"SAST $scanName $irxFile"'","EnablementMessage":"","FullyAutomatic":true}'| jq -r '. | {Id} | join(" ")');
+scanId=$(curl -s -k -X 'POST' "https://$serviceUrl/api/v4/Scans/Sast" -H 'accept:application/json' -H "Authorization:Bearer $appscanToken" -H 'Content-Type:application/json' -d '{"AppId":"'"$appId"'","ApplicationFileId":"'"$irxFileId"'","ClientType":"user-site","EnableMailNotification":false,"Execute":true,"Locale":"en","Personal":false,"ScanName":"'"SAST $scanName $irxFile"'","EnablementMessage":"","FullyAutomatic":true}'| jq -r '. | {Id} | join(" ")');
 echo "Scan started, scanId $scanId";
 
 echo "The scan name is $scanName and scanId is $scanId"
 echo $scanId > scanId.txt
 
 # Check status scan and keep it in loop until Ready status.
-scanStatus=$(curl -k -s -X 'GET' "https://$serviceUrl/api/v4/Scans/Sast/$scanId" -H 'accept:application/json' -H "Authorization:Bearer $asocToken" | jq -r '.LatestExecution | {Status} | join(" ")');
+scanStatus=$(curl -k -s -X 'GET' "https://$serviceUrl/api/v4/Scans/Sast/$scanId" -H 'accept:application/json' -H "Authorization:Bearer $appscanToken" | jq -r '.LatestExecution | {Status} | join(" ")');
 echo $scanStatus
 while true ; do 
-    scanStatus=$(curl -k -s -X 'GET' "https://$serviceUrl/api/v4/Scans/Sast/$scanId" -H 'accept:application/json' -H "Authorization:Bearer $asocToken" | jq -r '.LatestExecution | {Status} | join(" ")');
+    scanStatus=$(curl -k -s -X 'GET' "https://$serviceUrl/api/v4/Scans/Sast/$scanId" -H 'accept:application/json' -H "Authorization:Bearer $appscanToken" | jq -r '.LatestExecution | {Status} | join(" ")');
     if [ "$scanStatus" == "Running" ] || [ "$scanStatus" == "InQueue" ]; then
         echo $scanStatus
     elif [ "$scanStatus" == "Failed" ]; then
@@ -70,4 +70,4 @@ while true ; do
     sleep 60
 done
 
-curl -k -s -X 'GET' "https://$serviceUrl/api/v4/Account/Logout" -H 'accept: */*' -H "Authorization: Bearer $asocToken"
+curl -k -s -X 'GET' "https://$serviceUrl/api/v4/Account/Logout" -H 'accept: */*' -H "Authorization: Bearer $appscanToken"
